487d1f4c2292ad638201aeb80c4831d6ad1d5f92
Chima-Anason <tblynxx@gmail.com>
2023-04-14 14:54:40 +0800

    User implementation

---
+++ src/com/anagraceTech/car/Brand.java
@@ -0,0 +1,8 @@
+package com.anagraceTech.car;
+
+public enum Brand {
+    TESLA,
+    VW,
+    MERCEDES,
+    AUDI
+}
---
+++ src/com/anagraceTech/user/UserService.java
@@ -0,0 +1,23 @@
+package com.anagraceTech.user;
+
+import java.util.UUID;
+
+public class UserService {
+
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    public User getUserById(UUID id){
+        for (User user : userDao.getUsers()) {
+            if(id == user.getId()){
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
