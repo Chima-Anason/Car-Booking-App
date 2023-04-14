fe354f0d54a918d84e5b2d5885cfe94c97f06f65
Chima-Anason <tblynxx@gmail.com>
2023-04-14 17:45:46 +0800

    car implementation, Closes #2

--- src/com/anagraceTech/car/CarDao.java
+++ src/com/anagraceTech/car/CarDao.java
@@ -1,4 +1,20 @@
 package com.anagraceTech.car;
 
+import java.math.BigDecimal;
+
 public class CarDao {
+
+    private static final Car[] cars;
+
+    static {
+        cars = new Car[]{
+                new Car("1234", new BigDecimal("89.00"), Brand.TESLA, true),
+                new Car("5678", new BigDecimal("50.00"), Brand.AUDI, false),
+                new Car("5678", new BigDecimal("77.00"), Brand.MERCEDES, false),
+        };
+    }
+
+    public Car[] getAllCars(){
+        return cars;
+    }
 }
--- src/com/anagraceTech/car/CarService.java
+++ src/com/anagraceTech/car/CarService.java
@@ -1,4 +1,52 @@
 package com.anagraceTech.car;
 
 public class CarService {
+
+    private final CarDao carDao;
+
+    public CarService(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    public Car getCar(String regNumber) {
+        for (Car car : carDao.getAllCars()) {
+            if (regNumber.equals(car.getRegNumber())) {
+                return car;
+            }
+        }
+        throw new IllegalStateException(String.format("Car with reg-Number %s not found", regNumber));
+    }
+
+    public Car[] getAllElectricCars() {
+        int count = 0;
+        Car[] cars = carDao.getAllCars();
+
+//        if (cars.length == 0) {
+//            return new Car[0];
+//        }
+
+        for (Car car : cars) {
+            if(car.isElectric()){
+                count++;
+            }
+        }
+
+//        if (count == 0) {
+//            return new Car[0];
+//        }
+
+        Car[] electricCars = new Car[count];
+
+        int index = 0;
+
+        for (int i = 0; i < cars.length; i++) {
+            if(cars[i].isElectric()){
+                electricCars[index] = cars[i];
+                index++;
+            }
+        }
+
+        return electricCars;
+    }
+
 }
